d5eebf3ca89c968fd3d6e3259df4aa993b5f1b63
Luis Menacho <luisantonio.menacho@gmail.com>
2023-09-25 21:00:19 -0400

    se muestra [2,2,2] cuando se ananaliza el numero 8

--- src/primos.js
+++ src/primos.js
@@ -2,7 +2,7 @@ class FactoresPrimos{
     static generar(n){
         let primos = [];
         if(n > 1){
-            if(n%2 === 0){
+            while(n%2 === 0){
                 primos.push(2);
                 n /= 2;
             }
--- src/primos.test.js
+++ src/primos.test.js
@@ -20,4 +20,12 @@ describe("Numeros primos", () => {
   it("Debe devolver 2,3 cuando se le pasa 6", () => {
     expect(FactoresPrimos.generar(6)).toEqual([2,3]);
   });
+
+  it("Debe devolver 2,2,2 cuando se le pasa 8", () => {
+    expect(FactoresPrimos.generar(8)).toEqual([2,2,2]);
+  });
+
+  it("Debe devolver 3,3 cuando se le pasa 9", () => {
+    expect(FactoresPrimos.generar(8)).toEqual([3,3]);
+  });
 });
